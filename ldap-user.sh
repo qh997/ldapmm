e3514a2ece1b545e63adad68d4b9d776bf637573
Geng Shuang <gengs@neusoft.com>
2013-11-21 10:38:07 +0800

    ldap-user.sh verbose

--- ldap-user.sh
+++ ldap-user.sh
@@ -1,38 +1,52 @@
 #!/bin/bash
 
-ARGS=$(getopt -o m --long manager -n "$0" -- "$@")
-eval set -- "$ARGS"
-while true ; do
-    case "$1" in
-        -m|--manager) : ${manager:=1}; shift;;
-        --) shift; break;;
-        *) echo "Internal error."; exit 1;;
-    esac
-done
-
-for val in $(cat 'config'); do eval $val; done
-gp_name=${1:-${user_name}}
-
-if [ -z $manager ]; then
-    for name in $(ldapsearch -h "${server}" \
-                             -D "cn=${account}, ou=people, dc=${dc}, dc=internal" \
-                             -w "${password}" \
-                             -b "cn=${user_name},ou=people,dc=${dc},dc=internal" \
-                  | sed -n 's/memberOf: //p' \
-                  | awk -F',' '{print $1}' \
-                  | awk -F'=' '{print $2}')
-    do
-        printf "%-15s\n" ${name}
+function init {
+    ARGS=$(getopt -o mv --long manager --long verbose -n "$0" -- "$@")
+    eval set -- "$ARGS"
+    while true ; do
+        case "$1" in
+            -m|--manager) : ${manager:=1}; shift;;
+            -v|--verbose) : ${verbose:=1}; shift;;
+            --) shift; break;;
+            *) echo "Internal error."; exit 1;;
+        esac
     done
+
+    for val in $(cat 'config'); do eval $val; done
+    user_name=${1:-${user_name}}
+}
+
+function ldap_read {
+    ou=$1
+    ldapsearch -h "${server}" \
+               -D "cn=${account}, ou=people, dc=${dc}, dc=internal" \
+               -w "${password}" \
+               -b "cn=${user_name},ou=${ou},dc=${dc},dc=internal"
+}
+
+function ldap_user {
+    ldap_read 'people'
+}
+
+init $@
+
+user_universe=$(ldap_user)
+
+OLD_IFS=$IFS
+IFS=$(echo -en "\n")
+
+if [ ${verbose} ]; then
+    echo ${user_universe}
+elif [ ${manager} ]; then
+    echo ${user_universe} | sed -n 's/managedObjects: //p' \
+                          | awk -F',' '{print $1}' \
+                          | awk -F'=' '{print $2}' \
+                          | sort
 else
-    for name in $(ldapsearch -h "${server}" \
-                             -D "cn=${account}, ou=people, dc=${dc}, dc=internal" \
-                             -w "${password}" \
-                             -b "cn=${user_name},ou=people,dc=${dc},dc=internal" \
-                  | sed -n 's/managedObjects: //p' \
-                  | awk -F',' '{print $1}' \
-                  | awk -F'=' '{print $2}')
-    do
-        printf "%-15s\n" ${name}
-    done
+    echo ${user_universe} | sed -n 's/memberOf: //p' \
+                          | awk -F',' '{print $1}' \
+                          | awk -F'=' '{print $2}' \
+                          | sort
 fi
+
+IFS=${OLD_IFS}
